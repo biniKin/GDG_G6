077c17d87d5e586a8b76877a78868f22901eef75
Biniyam Kinfe <biniyamkinfe21@gmail.com>
2025-03-24 12:22:37 -0700

    removing task09

--- task09/lib/data/services/storage_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class StorageService {
-  Future<void> saveUserName(String name) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('username', name);
-  }
-}
